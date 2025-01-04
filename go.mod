6feab46553b2081f54aca5aac578ecfc908623a5
Dylan Mazurek <dylmazurek@gmail.com>
2025-01-04 15:08:24 +1100

    Remove deprecated test files and update dependencies

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 
 require (
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-jose/go-jose/v4 v4.0.4 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
@@ -22,11 +23,13 @@ require (
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
@@ -36,6 +39,7 @@ require (
 	github.com/markkurossi/tabulate v0.0.0-20230223130100-d4965869b123
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/stretchr/testify v1.10.0
 	github.com/zeebo/xxh3 v1.0.2
 	golang.org/x/sys v0.28.0 // indirect
 )
--- pkg/utilities/uuid/uuid_test.go
+++ pkg/utilities/uuid/uuid_test.go
@@ -18,23 +18,23 @@ func TestParsing(t *testing.T) {
 	}{
 		{
 			name:  "pass composed id to uuid",
-			input: "spaceship-portfolio-earth-b1365644-f8c4-4241-957d-a4abfb87b2e8",
+			input: "lunch-portfolio-test-b1365644-f8c4-4241-957d-a4abfb87b2e8",
 
-			wantUUIDStr:    "2874f671-65a1-80e1-4899-58935b29a48a",
+			wantUUIDStr:    "c2bac134-4002-5100-62b6-dd162d686eaf",
 			wantParsedType: uuid.ParseTypeComposed,
 		},
 		{
 			name:  "pass uuid to uuid",
-			input: "d09b1eb7-7ed6-4f1c-b7a1-50138688a644",
+			input: "d09b1ebb-7ed6-4f1c-b7a1-50138688a644",
 
-			wantUUIDStr:    "d09b1eb7-7ed6-4f1c-b7a1-50138688a644",
+			wantUUIDStr:    "d09b1ebb-7ed6-4f1c-b7a1-50138688a644",
 			wantParsedType: uuid.ParseTypeUUID,
 		},
 		{
 			name:  "pass raw uuid to uuid",
-			input: "b1365644-f8c4-4241-957d-a4abfb87b2e8",
+			input: "b1365644-f8cc-4241-957d-a4abfb87b2e8",
 
-			wantUUIDStr:    "b1365644-f8c4-4241-957d-a4abfb87b2e8",
+			wantUUIDStr:    "b1365644-f8cc-4241-957d-a4abfb87b2e8",
 			wantParsedType: uuid.ParseTypeUUID,
 		},
 		{
@@ -64,7 +64,7 @@ func TestParsing(t *testing.T) {
 				t.Errorf("test %q: got %q, want %q", test.input, *uuidString, test.wantUUIDStr)
 			}
 
-			if uuid.ParsedUsing != test.wantParsedType {
+			if uuid != nil && uuid.ParsedUsing != test.wantParsedType {
 				t.Errorf("test %q: got %q, want %q", test.input, uuid.ParsedUsing, test.wantParsedType)
 			}
 		})
